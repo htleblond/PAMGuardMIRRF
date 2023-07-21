fd8eefc9c6ebdbbdf4bb350cf5aeffdaf231d63d
htleblond <wtleblond@gmail.com>
2023-07-20 22:38:44 -0700

    1.01

---
+++ PamGuardMIRRF/downloads/1.01/mirrf_install_1.01.bat
@@ -0,0 +1,14 @@
+python.exe -m pip install --upgrade --user pip
+pip install --upgrade numpy
+pip install --upgrade librosa
+pip install --upgrade soundfile
+pip install --upgrade matplotlib
+pip install --upgrade scipy
+pip install --upgrade IPython
+pip install --upgrade colour
+pip install --upgrade num2words
+pip install --upgrade datetime
+pip install --upgrade pytz
+pip install --upgrade logging
+pip install --upgrade pandas
+pip install --upgrade sklearn
