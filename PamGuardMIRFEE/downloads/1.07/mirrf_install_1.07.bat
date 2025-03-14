9b29de12d96889839bd7b48a4b29b4525ec70995
htleblond <wtleblond@gmail.com>
2025-03-13 17:31:45 -0700

    Adding 1.07a .jar to downloads

---
+++ PamGuardMIRFEE/downloads/1.07/mirrf_install_1.07.bat
@@ -0,0 +1,15 @@
+python.exe -m pip install --upgrade --user pip
+pip install --upgrade --user numpy
+pip install --upgrade --user librosa
+pip install --upgrade --user soundfile
+pip install --upgrade --user matplotlib
+pip install --upgrade --user scipy
+pip install --upgrade --user IPython
+pip install --upgrade --user colour
+pip install --upgrade --user num2words
+pip install --upgrade --user datetime
+pip install --upgrade --user pytz
+pip install --upgrade --user logging
+pip install --upgrade --user pandas
+pip install --upgrade --user scikit-learn
+pip install --upgrade --user praat-parselmouth
